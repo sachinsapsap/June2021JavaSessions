ed9180aaacdb98c31770bc4dd26655119656e75c
sachinsapsap <sachinsapsap@gmail.com>
2021-08-28 22:25:24 +0530

    added and updated the code

--- src/BuilderPattern/Shopping.java
+++ src/BuilderPattern/Shopping.java
@@ -6,7 +6,7 @@ public class Shopping {
 
 
 		EcommApp ecom= new EcommApp();
-		ecom.login("sachinsapsap@gmail.com", "test123")
+		ecom.login("sachinsapsap123@gmail.com", "test1234")
 		              .search("Tshirts", "Black")
 		                 .addToCart("Tshirts")
 		                     .doPayment("1212hdfc.com", 1234, "test")
